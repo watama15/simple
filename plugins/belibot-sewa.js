c9d97cc60b34484156ca8584b05dc895c8147a4c
watama15 <109936711+watama15@users.noreply.github.com>
2022-11-10 00:36:35 +0700

    Update belibot-sewa.js

--- plugins/belibot-sewa.js
+++ plugins/belibot-sewa.js
@@ -17,7 +17,7 @@ let gh = 'https://github.com/koncit'
 'body':'© Anya Bot',
 'thumbnail':await(await fetch('https://i.ibb.co/vzrsS1w/thumbnail.jpg')).buffer(),
 'sourceUrl':gh}},
-'caption':'Hi, Silahkan pilih opsi di bawah ini
+'caption':'Hi, Silahkan pilih opsi di bawah ini\n
 ╭━━━━「 *SEWA* 」
 ┊⫹⫺ *Hemat:* _5k/grup (1 minggu)_
 ┊⫹⫺ *Normal:* _15k/grup (1 bulan)_
